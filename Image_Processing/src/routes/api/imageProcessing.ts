4abeef4898c56c2d1d025bf447f9cf17cd64a52f
Kumar <akumar62@visteon.com>
2022-07-21 00:55:06 +0530

    removed unnecessary console log

--- Image_Processing/src/routes/api/imageProcessing.ts
+++ Image_Processing/src/routes/api/imageProcessing.ts
@@ -14,15 +14,12 @@ const img :string= String(req.query.img);
 const height: number = Number(req.query.height);
 const width: number = Number(req.query.width);
 const workDirectory=path.resolve(__dirname,'../../..');
-console.log(workDirectory);
 const imagesDir=workDirectory+'/images';
-console.log(imagesDir);
 const imageName=imagesDir+'/'+img;
 if(!fs.existsSync(imageName)){
     res.send('<h1>Image not found</h1>');
   }
 const processed_imagesDir=workDirectory+'/processed_images';
-console.log(processed_imagesDir);
 try {
     if (!fs.existsSync(processed_imagesDir+'/'+height+width+img)) {
         await imageResize(imagesDir+'/'+img,height, width,processed_imagesDir+'/'+height+width+img);
